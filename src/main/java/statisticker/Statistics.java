3dd2087a872d531349c47d83c55fee8bb3b6cf61
Linkesh S <linkesh.s.1111@gmail.com>
2022-05-21 23:55:46 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -66,7 +66,21 @@ public class Statistics
             this.ledGlows=true;
         }
     }
-    
+    public static class StatsChecker{
+        Float maxThresholds;
+        IAlerter alerters[];
+    StatsChecker(Float maxThresholds, IAlerter alerters[]){
+        this.maxThresholds=maxThresholds;
+        this.alerters[]=alerters[];
+    }
+        public checkAndAlert(List<Float> numbers){
+        Collections.sort(numbers);
+            if(this.maxThresholds < numbers.get((numbers.size()-1))){
+            this.alerters[0].setTrue();
+            this.alerters[1].setTrue();
+            }
+        }
+    }
     
     }
 
