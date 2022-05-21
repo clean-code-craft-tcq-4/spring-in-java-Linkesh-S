df618794b90f850280ae7ced936075e5a5bab63b
Linkesh S <linkesh.s.1111@gmail.com>
2022-05-21 23:16:36 +0530

    Update IAlerter.java

--- src/main/java/statisticker/IAlerter.java
+++ src/main/java/statisticker/IAlerter.java
@@ -3,7 +3,7 @@ package statisticker;
 import java.util.List;
 import java.util.*;
 
-public static class IAlerter extends LEDAlert, EmailAlert {
+public  class IAlerter extends LEDAlert, EmailAlert {
   
 
 }
