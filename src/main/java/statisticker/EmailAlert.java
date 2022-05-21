9c8c1ad95e8c713aff9e1eddc3f5cb4d9aaae7b0
Linkesh S <linkesh.s.1111@gmail.com>
2022-05-22 00:04:23 +0530

    Update EmailAlert.java

--- src/main/java/statisticker/EmailAlert.java
+++ src/main/java/statisticker/EmailAlert.java
@@ -3,7 +3,7 @@ package statisticker;
 import java.util.List;
 import java.util.*;
 
-public static class EmailAlert implements IAlerter{
+public class EmailAlert implements IAlerter{
         boolean ledGlows;
         @Override
         public void setTrue(){
